fb3d5950e030b1de65f4f39d6cff91e2fe39226b
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-06-26 19:04:36 +0200

    implementing the movies listing per users

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -52,7 +52,7 @@ class Comment extends Model
 
     public static function JoinCommentAndUser($movieId) {
 
-      return self::select("*", "users.created_at as user_created_at", "users.updated_at as user_updated_at")
+      return self::select("comments.*", "users.created_at as user_created_at", "users.updated_at as user_updated_at")
                           ->where("movie_id", "=", $movieId)
                           ->join("users", "users.id", "=", "comments.user_id")->orderByDesc("created_at")->get();
     }
--- database/seeders/ListingTableSeeder.php
+++ database/seeders/ListingTableSeeder.php
@@ -8,16 +8,52 @@ use Illuminate\Support\Facades\DB;
 class ListingTableSeeder extends Seeder
 {
     public function run()
-    {
+    {   
+        DB::table('listing_movies')->insert([
+            'status' => 'Vus',
+            'movie_id' => 11, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+        DB::table('listing_movies')->insert([
+            'status' => 'Vus',
+            'movie_id' => 35, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+
+        DB::table('listing_movies')->insert([
+            'status' => 'Vus',
+            'movie_id' => 71, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+
         DB::table('listing_movies')->insert([
             'status' => 'À voir',
-            'movie_id' => 5, // Assurez-vous que le film avec l'ID 1 existe
-            'user_id' => 2, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+            'movie_id' => 38, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
         ]);
         DB::table('listing_movies')->insert([
             'status' => 'À voir',
-            'movie_id' => 6, // Assurez-vous que le film avec l'ID 1 existe
-            'user_id' => 2, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+            'movie_id' => 73, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
         ]);
+        DB::table('listing_movies')->insert([
+            'status' => 'À voir',
+            'movie_id' => 77, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+
+        DB::table('listing_movies')->insert([
+            'status' => 'À voir',
+            'movie_id' => 78, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+
+        DB::table('listing_movies')->insert([
+            'status' => 'À voir',
+            'movie_id' => 111, // Assurez-vous que le film avec l'ID 1 existe
+            'user_id' => 1, // Assurez-vous que l'utilisateur avec l'ID 1 existe
+        ]);
+
+
     }
 }
