96a16521f2e15041bf5c786101fa34e1dca9193e
Mael38 <mael.belhacene@dhomplus.fr>
2024-07-25 14:03:51 +0200

    Nice

--- app/Http/Controllers/RecommendationController.php
+++ app/Http/Controllers/RecommendationController.php
@@ -10,7 +10,7 @@ class RecommendationController extends Controller
 {
     public function showForm()
     {
-        // Afficher la vue pour le formulaire d'entrée du genre de film
+        
         return view('recommendation-form');
     }
 
@@ -18,20 +18,20 @@ class RecommendationController extends Controller
     {
         $genre = $request->input('genre');
 
-        // URL de l'API Hugging Face et headers requis
+        
         $apiUrl = 'https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct';
         $headers = [
             'Authorization' => 'Bearer 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', // Remplacez YOUR_API_KEY par votre clé d'API Hugging Face valide
             'Content-Type' => 'application/json',
         ];
 
-        // Données JSON à envoyer à l'API Hugging Face
+       
         $requestData = [
-            'inputs' => "Donne-moi quelques recommandations de films dans le genre '$genre', en français, s'il te plaît.",
+            'inputs' => "Donne-moi quelques recommandations de films dans le genre '$genre', parle en français, s'il te plaît et tu répond via ce format = Bienvenue sur le site MovieShelter voici quelques recommandations : .",
         ];
 
         try {
-            // Effectuer la requête POST à l'API Hugging Face
+           
             $client = new Client();
             $response = $client->post($apiUrl, [
                 'headers' => $headers,
@@ -63,7 +63,6 @@ class RecommendationController extends Controller
                 dd('Erreur inconnue lors de la requête à l\'API Hugging Face.');
             }
         } catch (\Exception $e) {
-            // Gérer les autres exceptions
             dd($e->getMessage());
         }
     }
