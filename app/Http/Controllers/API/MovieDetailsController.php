c99abe5be589d2dc74158f882e874ae702f507a3
jimmy <jimmy@localhost>
2024-07-25 13:25:00 +0200

    Some basic routes for the API, only GET is supported so far call using "api" as prefix, exemple: "api/users/index" for "users/index" route

---
+++ app/Http/Controllers/API/MovieDetailsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class MovieDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $movie = Movie::ById($id);
+        if($movie == null) {
+          abort(404);
+        } 
+        $comments = Comment::JoinCommentAndUser($movie->id);
+        
+        if(Auth::check()) {
+          $current_user = Auth::user(); 
+        } else {
+          $current_user = null;
+        }
+  
+        return response()->json([$movie, $comments, $current_user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Movie $movie)
+    {
+        //
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
-
-
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
