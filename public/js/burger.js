15322ac27c4a2a1ddd80df0dad8eb949a73f6bdf
Eliott <eliottcheminal@gmail.com>
2024-07-26 16:03:00 +0200

    Menu burger for responsive header

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,20 +1,5 @@
-var sidenav = document.getElementById("rightSection");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  sidenav.classList.remove("active");
-}
-
-sidenav.childNodes.forEach((child) => {
-  child.hidden = true;
-});
+document.getElementById("burgerMenuButton").addEventListener("click", function() {
+    document.getElementById("burgerMenu").classList.toggle("active");
+  });
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src ="{{ asset('js/burger.js') }}"></script>
+
     <title><?php 
         use Illuminate\Support\Facades\Route;
         echo Route::currentRouteName() ?></title>
@@ -82,12 +82,61 @@
             justify-content: space-around;
         }
 
-        @media screen and (max-width: 870px) {
+        @media screen and (max-width: 900px) {
             .rightSection {
                 flex-direction: column;
                 align-items: center;
             }
         }
+
+        button {
+            background-color: #333;
+            color: white;
+            border: none;
+            font-size: 1.5em;
+            cursor: pointer;
+             
+        }
+
+        .burger-menu {
+        display: none;
+        }
+
+        .burger-menu.active {
+         display: flex;
+         flex-direction:column;
+         position: absolute;
+         width: 100px;
+            top: 60px;
+            right: 0;
+         
+        background-color: #333;
+         box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+    }
+
+    @media (min-width: 900px) {
+  .burgersection {
+    display: none;
+  
+  }
+}
+
+  @media (max-width: 900px) {
+  .links{
+    display: none;
+  
+  }
+}
+*/.burgerking {
+            background-color: #333;
+            color: black;
+            font-size: 1em;
+            flex-grow: 1;
+            text-align: center;
+            padding: 1em 0;
+            text-decoration: none;
+            white-space: nowrap;
+}
     </style>
 </head>
 <body>
@@ -113,22 +162,27 @@
         @endif
     </div>
     <div class="burgersection">
-        <a class="links" href="/home">Accueil</a>
-        <a class="links" href="/search">Recherche</a>
+    <button id="burgerMenuButton">☰</button>
+    <nav id="burgerMenu" class="burger-menu">
+        <a class="burgerking" href="/home">Accueil</a>
+        <a class="burgerking" href="/search">Recherche</a>
        <?php
         $user = Auth::check();
         ?>
         @if($user)     
-            <a class="links" href="/list/{{Auth::id()}}">Ma liste</a>
-            <a class="links" href="{{ route('profile.show', ['name' => Auth::user()->name]) }}">Profil</a>
-            <a class="links" href="/logout">Deconnexion</a>
+            <a class="burgerking" href="/list/{{Auth::id()}}">Ma liste</a>
+            <a class="burgerking" href="{{ route('profile.show', ['name' => Auth::user()->name]) }}">Profil</a>
+            <a class="burgerking" href="/logout">Deconnexion</a>
         @else
-            <a class="links" href="/login">Connexion</a>
-            <a class="links" href="/register">Inscription</a>
+            <a class="burgerking" href="/login">Connexion</a>
+            <a class="burgerking" href="/register">Inscription</a>
         @endif
+        
     </div>
+    </nav> 
 </header> 
-</nav>  
+
 <br>
+<script src="{{ asset('js/burger.js') }}"></script>">
 </body>
 </html>
