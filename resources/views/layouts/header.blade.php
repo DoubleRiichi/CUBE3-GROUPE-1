786e736000405bd991d14a13e1b4b69cac3c11f6
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-09-06 15:48:13 +0200

    added header to recommendation page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?php 
-        use Illuminate\Support\Facades\Route;
-        echo Route::currentRouteName() ?></title>
+    <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body {
@@ -98,7 +96,7 @@
             right: 0;
             background-color: #35607c;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            border-radius: 10px; 
+            border-radius: 10px;
         }
 
         @media (min-width: 900px) {
