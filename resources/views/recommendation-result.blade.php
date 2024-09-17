ac2024e1788cd4b57c94c4c2471856e454c3da80
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-09-17 10:44:32 +0200

    css improvments

--- resources/views/recommendation-result.blade.php
+++ resources/views/recommendation-result.blade.php
@@ -3,10 +3,12 @@
 @section('content')
 
 <link rel="stylesheet" href="{{ asset('css/recommendation.css') }}">
-<div class="mainbox">
-    <h1>Résultat des recommandations</h1>
-    <p>{{ $recommendationText }}</p>
-    <a href="{{ route('showForm') }}" class="btn bluebtn">Revenir au formulaire</a>
+<div class="row">
+    <div class="col col-md-4 mx-auto bg-light mt-4 p-3 text-center">
+        <h1>Résultat des recommandations</h1>
+        <p>{{ $recommendationText }}</p>
+        <a href="{{ route('showForm') }}" class="btn bluebtn">Revenir au formulaire</a>
+    </div>
 </div>
 </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,48 +2,51 @@
 @section ('title', 'Inscription')
 @section('content')
 <link rel="stylesheet" href="{{ asset("css/register.css")}}">
-<div class="mainbox" id="register">
-    @if ($errors->any())
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+<div class="row" id="register">
+    <div class="col col-md-4 mx-auto bg-light mt-4 p-3 text-center">
+
+        @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form class="inline-form" method="POST" action="{{ route('register') }}">
+            <h1>Créer un compte</h1>
+
+            @csrf
+            <div>
+                <label for="name">Nom Prénom</label> <br>
+                <input type="text" id="name" name="name" value="{{ old('name') }}" required>
+            </div>
+            <div>
+                <label for="email">Email</label><br>
+                <input type="email" id="email" name="email" value="{{ old('email') }}" required>
+            </div>
+            <div>
+                <label for="password">Mot de passe</label><br>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div>
+                <label for="password_confirmation">Confirmez mot de passe</label><br>
+                <input type="password" id="password_confirmation" name="password_confirmation" required>
+            </div>
+            <div>
+                <label for="username">Pseudo</label><br>
+                <input type="text" id="username" name="username" value="{{ old('username') }}" required>
+            </div>
+            <div>
+                <button class="bluebtn" type="submit">S'inscrire</button>
+            </div>
+
+        </form>
+        <a href="{{ route('auth.google') }}" class="btn googlebtn">
+            <img src="{{ asset('Assets/google-logo.png') }}" alt="Google logo">Google
+        </a>
     </div>
-    @endif
-
-    <form class="inline-form" method="POST" action="{{ route('register') }}">
-        <h1>Créer un compte</h1>
-
-        @csrf
-        <div>
-            <label for="name">Name</label> <br>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" required>
-        </div>
-        <div>
-            <label for="email">Email</label><br>
-            <input type="email" id="email" name="email" value="{{ old('email') }}" required>
-        </div>
-        <div>
-            <label for="password">Password</label><br>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <div>
-            <label for="password_confirmation">Confirm Password</label><br>
-            <input type="password" id="password_confirmation" name="password_confirmation" required>
-        </div>
-        <div>
-            <label for="username">Username</label><br>
-            <input type="text" id="username" name="username" value="{{ old('username') }}" required>
-        </div>
-        <div>
-            <button class="bluebtn" type="submit">S'inscrire</button>
-        </div>
-
-    </form>
-    <a href="{{ route('auth.google') }}" class="btn googlebtn">
-        <img src="{{ asset('Assets/google-logo.png') }}" alt="Google logo">Google
-    </a>
 </div>
 @endsection
